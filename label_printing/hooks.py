04690c56419f5cff6849e8eb0c785cd563764530
Gaurang1979 <126887545+Gaurang1979@users.noreply.github.com>
2026-09-08 15:08:23 +0530

    Add Frappe hooks

---
+++ label_printing/hooks.py
@@ -0,0 +1,16 @@
+app_name = "label_printing"
+app_title = "Label Printing"
+app_publisher = "Sundaram Technologies"
+app_description = "ERPNext Zebra ZPL-II label printing"
+app_email = ""
+app_license = "GPL-3.0"
+
+required_apps = ["frappe", "erpnext"]
+
+fixtures = []
+
+permission_query_conditions = {}
+
+scheduler_events = {
+    "all": []
+}
