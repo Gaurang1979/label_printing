748fcb4b31b6ef587fa2f3601dd6c10c02a3038c
Gaurang1979 <126887545+Gaurang1979@users.noreply.github.com>
2026-09-08 15:46:36 +0530

    Fix serial label reprint workflow

--- label_printing/public/js/serial_no.js
+++ label_printing/public/js/serial_no.js
@@ -2,22 +2,9 @@ frappe.ui.form.on('Serial No', {
     refresh(frm) {
         if (!frm.doc.name) return;
         frm.add_custom_button(__('Reprint Label'), () => {
-            const d = new frappe.ui.Dialog({
-                title: __('Reprint Serial Label'),
-                fields: [{fieldname:'reason', fieldtype:'Data', label:__('Reason'), reqd:1}],
-                primary_action_label: __('Create Reprint Job'),
-                primary_action(values) {
-                    frappe.call({
-                        method: 'label_printing.print_api.find_reprint_source',
-                        args: {serial_no: frm.doc.name},
-                        callback(r) {
-                            if (!r.message) return frappe.msgprint(__('No printable source document was found for this serial.'));
-                            label_printing.print_job({doc: frm.doc, doctype: 'Serial No'}, [frm.doc.name], values.reason, r.message).then(() => d.hide());
-                        }
-                    });
-                }
-            });
-            d.show();
+            const d = new frappe.ui.Dialog({title:__('Reprint Serial Label'),fields:[{fieldname:'reason',fieldtype:'Data',label:__('Reason'),reqd:1}],primary_action_label:__('Create Reprint Job'),primary_action(values){
+                frappe.call({method:'label_printing.print_api.create_reprint_job',args:{serial_no:frm.doc.name,reprint_reason:values.reason},callback:r=>{if(r.message){d.hide();frappe.show_alert({message:__('Reprint Job {0} created',[r.message]),indicator:'green'});}}});
+            }}); d.show();
         }, __('Labels'));
     }
 });
