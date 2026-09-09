347af8af79ee95fa2bf62098187798a6d9719ce6
Gaurang1979 <126887545+Gaurang1979@users.noreply.github.com>
2026-09-09 16:29:18 +0530

    Remove global Printing workspace shortcut injection

--- label_printing/hooks.py
+++ label_printing/hooks.py
@@ -7,7 +7,6 @@ app_license = "GPL-3.0"
 required_apps = ["frappe", "erpnext"]
 app_include_js = ["/assets/label_printing/js/manage_printer.js", "/assets/label_printing/js/browser_print.js", "/assets/label_printing/js/label_printing.js", "/assets/label_printing/js/label_print_job.js"]
 after_install = "label_printing.install.after_install"
-after_migrate = ["label_printing.install.ensure_printing_workspace_shortcuts"]
 
 doctype_js = {
     "Purchase Receipt": "public/js/purchase_receipt.js",
